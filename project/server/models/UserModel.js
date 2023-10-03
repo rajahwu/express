9fe8e4cc11805bfa9e4227a1de8121e28080e5dd
rajahwu <vmradford@gmail.com>
2023-10-03 16:21:14 -0400

    add user model

---
+++ project/server/models/UserModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const EmailValidator = require('email-validator');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 12
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        index: { unique: true },
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        index: { unique: true },
+        validate: {
+            validator: EmailValidator.validate,
+            message: props => `${props.value} is not a valid email address`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        index: { unique: true },
+        minlength: 8
+    }
+}, {
+    timestamps: true,
+});
+
+UserSchema.pre('save', async function preSave(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    try {
+        const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+        user.password = hash;
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+UserSchema.methods.comparePassword = async function comparePassword(candidate) {
+    return bcrypt.compare(candidate, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
